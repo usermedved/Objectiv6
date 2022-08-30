2b2448f9739784b5c3749061de59f4da2faa4965
Sergei Medvedev <clip1988@yandex.ru>
2022-08-30 23:58:34 +0300

    Objective 41 project

---
+++ Objective41/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Введите колличество чисел: ");
+int sizeM = Convert.ToInt32(Console.ReadLine());
+int[] numbersM = new int[sizeM];
+int count = 0;
+for (int i = 0; i < numbersM.Length; i++)
+{
+    Console.Write("Введите число: ");
+    numbersM[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+int Comparison(int[] numbersM)
+{
+    for (int i = 0; i < numbersM.Length; i++)
+    {
+        if (numbersM[i] > 0) count += 1;
+        
+    }
+return count;
+}
+
+Console.WriteLine($"Колличество цифр больше 0: {Comparison(numbersM)}");
