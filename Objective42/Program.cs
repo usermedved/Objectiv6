ee992bacb149e2069d298d53aea82c710ccec82d
Sergei Medvedev <clip1988@yandex.ru>
2022-08-31 00:00:58 +0300

    Objective 42 project

---
+++ Objective42/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите десятичное число: ");
+int decimalNumber = Convert.ToInt32(Console.ReadLine());
+int[] arraybinaryNumber = new int[10]; 
+int binaryNumber = 0; 
+
+Console.Write("Заданное десятичное число преобразованное в двоичное: ");
+for (int i = 0; decimalNumber >= 1; i++)
+{
+    binaryNumber = decimalNumber % 2;
+    decimalNumber = decimalNumber / 2;
+    Console.Write(binaryNumber);
+}
